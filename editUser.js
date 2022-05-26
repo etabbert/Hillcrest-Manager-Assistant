f5252ce4dd09d60786333885ca3a1674554e005f
etabbert <etabbert@uiowa.edu>
2022-05-26 12:00:49 -0500

    Edit user features

--- editUser.js
+++ editUser.js
@@ -57,3 +57,196 @@ async function editUser() {
 	if (result.sundayStart){document.getElementById("sundayStartText").innerHTML = "Sunday</br>Start: " + String(result.sundayStart);}
 	if (result.sundayStop){document.getElementById("sundayStopText").innerHTML = "Stop: " + String(result.sundayStop) + "</br></br>";}
 }
+
+async function applyEdits() {
+	
+	var studentid = gup("ID");
+
+	if (document.getElementById('firstname').value) {
+		firstname = document.getElementById('firstname').value;
+	} else {
+		firstname = null;
+	}
+
+	if (document.getElementById('lastname').value) {
+		lastname = document.getElementById('lastname').value;
+	} else {
+		lastname = null;
+	}
+
+	if (document.getElementById('phone').value) {
+		inputPhone = document.getElementById('phone').value;
+		phoneClean = ('' + inputPhone).replace(/\D/g, '');
+		if (phoneClean.length == 10) {
+			phone = phoneClean;
+		} else {
+			document.getElementById('error').innerHTML = "Please enter a 10 digit phone number";
+		}
+	} else {
+		phone = null;
+	}
+
+	
+	if (document.getElementById('email').value) {
+		email = document.getElementById('email').value;
+	} else {
+		email = null;
+	}
+	
+	if (document.querySelector('input[name="checker"]:checked')) {
+		checker = true;
+	} else {
+		checker = null;
+	}
+	
+	if (document.querySelector('input[name="role"]:checked')) {
+		role = document.querySelector('input[name="role"]:checked').value;
+	} else {
+		role = null;
+	}
+	
+	
+	if (document.getElementById('mondayStart').value) {
+		mondayStartInput = document.getElementById('mondayStart').value;
+		var mondayStartClean = ('' + mondayStartInput).replace(/\D/g, '');
+		var mondayStart = setStart(mondayStartClean);
+	} else {
+		mondayStart = null;
+	}
+	
+	if (document.getElementById('mondayStop').value) {
+		mondayStopInput = document.getElementById('mondayStop').value;
+		var mondayStopClean = ('' + mondayStopInput).replace(/\D/g, '');
+		var mondayStop = parseInt(setStop(mondayStopClean), 10);
+	} else {
+		mondayStop = null;
+	}
+	
+	if (document.getElementById('tuesdayStart').value) {
+		tuesdayStartInput = document.getElementById('tuesdayStart').value;
+		var tuesdayStartClean = ('' + tuesdayStartInput).replace(/\D/g, '');
+		var tuesdayStart = setStart(tuesdayStartClean);
+	} else {
+		tuesdayStart = null;
+	}
+	
+	if (document.getElementById('tuesdayStop').value) {
+		tuesdayStopInput = document.getElementById('tuesdayStop').value;
+		tuesdayStopClean = ('' + tuesdayStopInput).replace(/\D/g, '');
+		tuesdayStop = setStop(tuesdayStopClean);
+	} else {
+		tuesdayStop = null;
+	}
+	
+	if (document.getElementById('wednesdayStart').value) {
+		wednesdayStartInput = document.getElementById('wednesdayStart').value;
+		var wednesdayStartClean = ('' + wednesdayStartInput).replace(/\D/g, '');
+		var wednesdayStart = setStart(wednesdayStartClean);
+	} else {
+		wednesdayStart = null;
+	}
+	
+	if (document.getElementById('wednesdayStop').value) {
+		wednesdayStopInput = document.getElementById('wednesdayStop').value;
+		var wednesdayStopClean = ('' + wednesdayStopInput).replace(/\D/g, '');
+		var wednesdayStop = setStop(wednesdayStopClean);
+	} else {
+		wednesdayStop = null;
+	}
+	
+	if (document.getElementById('thursdayStart').value) {
+		thursdayStartInput = document.getElementById('thursdayStart').value;
+		var thursdayStartClean = ('' + thursdayStartInput).replace(/\D/g, '');
+		var thursdayStart = setStart(thursdayStartClean);
+	} else {
+		thursdayStart = null;
+	}
+	
+	if (document.getElementById('thursdayStop').value) {
+		thursdayStopInput = document.getElementById('thursdayStop').value;
+		var thursdayStopClean = ('' + thursdayStopInput).replace(/\D/g, '');
+		var thursdayStop = setStop(thursdayStopClean);
+	} else {
+		thursdayStop = null;
+	}
+
+	if (document.getElementById('fridayStart').value) {
+		fridayStartInput = document.getElementById('fridayStart').value;
+		var fridayStartClean = ('' + fridayStartInput).replace(/\D/g, '');
+		var fridayStart = setStart(fridayStartClean);
+	} else {
+		fridayStart = null;
+	}
+	
+	if (document.getElementById('fridayStop').value) {	
+		fridayStopInput = document.getElementById('fridayStop').value;
+		var fridayStopClean = ('' + fridayStopInput).replace(/\D/g, '');
+		var fridayStop = setStop(fridayStopClean);
+	} else {
+		fridayStop = null;
+	}
+	
+	if (document.getElementById('saturdayStart').value) {
+		saturdayStartInput = document.getElementById('saturdayStart').value;
+		var saturdayStartClean = ('' + saturdayStartInput).replace(/\D/g, '');
+		var saturdayStart = setStart(saturdayStartClean);
+	} else {
+		saturdayStart = null;
+	}
+	
+	if (document.getElementById('saturdayStop').value) {
+		saturdayStopInput = document.getElementById('saturdayStop').value;
+		var saturdayStopClean = ('' + saturdayStopInput).replace(/\D/g, '');
+		var saturdayStop = setStop(saturdayStopClean);
+	} else {
+		saturdayStop = null;
+	}
+	
+	if (document.getElementById('sundayStart').value) {	
+		sundayStartInput = document.getElementById('sundayStart').value;
+		var sundayStartClean = ('' + sundayStartInput).replace(/\D/g, '');
+		var sundayStart = setStart(sundayStartClean);
+	} else {
+		sundayStart = null;
+	}
+	
+	if (document.getElementById('sundayStop').value) {
+		sundayStopInput = document.getElementById('sundayStop').value;
+		var sundayStopClean = ('' + sundayStopInput).replace(/\D/g, '');	
+		var sundayStop = setStop(sundayStopClean);
+	} else {
+		sundayStop = null;
+	}
+	
+	let myResponse = await fetch("editUser.php", {
+	method: 'POST',
+	headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
+	body: JSON.stringify({firstname: firstname, lastname: lastname, studentid: studentid, phone: phone, email: email, checker: checker, role: role, mondayStart: mondayStart, mondayStop: mondayStop, tuesdayStart: tuesdayStart, tuesdayStop: tuesdayStop, wednesdayStart: wednesdayStart, wednesdayStop: wednesdayStop, thursdayStart: thursdayStart, thursdayStop: thursdayStop, fridayStart: fridayStart, fridayStop: fridayStop, saturdayStart: saturdayStart, saturdayStop: saturdayStop, sundayStart: sundayStart, sundayStop: sundayStop})
+	});
+	
+	let result = await myResponse.json();
+    let output = JSON.stringify(result);
+	console.log(result);
+	
+	if (result == "Complete") {
+		editUser();
+		document.getElementById('status').innerHTML = "Changes submitted successfully!</br></br>";
+		window.scrollTo(0, 0);
+	}
+}
+
+function setStart(start) {
+	if (parseInt(start) < 600) {
+		return '600';
+	} else {
+		return start;
+	}
+}
+
+function setStop(stop) {
+	if (parseInt(stop) > 2000) {
+		return '2000';
+	} else {
+		return stop;
+	}
+}
---
+++ editUser.php
@@ -0,0 +1,133 @@
+<?php
+function applyUserEdits($firstname, $lastname, $studentid, $phone, $email, $checker, $role, $mondayStart, $mondayStop, $tuesdayStart, $tuesdayStop, $wednesdayStart, $wednesdayStop, $thursdayStart, $thursdayStop, $fridayStart, $fridayStop, $saturdayStart, $saturdayStop, $sundayStart, $sundayStop) {
+	$db = new SQLite3('db/employees.db');
+	$db->exec('BEGIN EXCLUSIVE;');
+	error_log($firstname);
+	$studentQuery = "SELECT * FROM employees WHERE studentid = '$studentid'";
+	$getStudent = $db->query($studentQuery);
+
+	$updateFirstname = "UPDATE employees SET firstname = '$firstname' WHERE studentid = '$studentid'";
+	$updateLastname = "UPDATE employees SET lastname = '$lastname' WHERE studentid = '$studentid'";
+	$updateStudentid = "UPDATE employees SET studentid = '$studentid' WHERE studentid = '$studentid'";
+	$updatePhone = "UPDATE employees SET phone = '$phone' WHERE studentid = '$studentid'";
+	$updateEmail = "UPDATE employees SET email = '$email' WHERE studentid = '$studentid'";
+	$updateChecker = "UPDATE employees SET checker = '$checker' WHERE studentid = '$studentid'";
+	$updateRole = "UPDATE employees SET role = '$role' WHERE studentid = '$studentid'";
+	$updateMondayStart = "UPDATE employees SET mondayStart = '$mondayStart' WHERE studentid = '$studentid'";
+	$updateMondayStop = "UPDATE employees SET mondayStop = '$mondayStop' WHERE studentid = '$studentid'";
+	$updateTuesdayStart = "UPDATE employees SET tuesdayStart = '$tuesdayStart' WHERE studentid = '$studentid'";
+	$updateTuesdayStop = "UPDATE employees SET tuesdayStop = '$tuesdayStop' WHERE studentid = '$studentid'";
+	$updateWednesdayStart = "UPDATE employees SET wednesdayStart = '$wednesdayStart' WHERE studentid = '$studentid'";
+	$updateWednesdayStop = "UPDATE employees SET wednesdayStop = '$wednesdayStop' WHERE studentid = '$studentid'";
+	$updateThursdayStart = "UPDATE employees SET thursdayStart = '$thursdayStart' WHERE studentid = '$studentid'";
+	$updateThursdayStop = "UPDATE employees SET thursdayStop = '$thursdayStop' WHERE studentid = '$studentid'";
+	$updateFridayStart = "UPDATE employees SET fridayStart = '$fridayStart' WHERE studentid = '$studentid'";
+	$updateFridayStop = "UPDATE employees SET fridayStop = '$fridayStop' WHERE studentid = '$studentid'";
+	$updateSaturdayStart = "UPDATE employees SET saturdayStart = '$saturdayStart' WHERE studentid = '$studentid'";
+	$updateSaturdayStop = "UPDATE employees SET saturdayStop = '$saturdayStop' WHERE studentid = '$studentid'";
+	$updateSundayStart = "UPDATE employees SET sundayStart = '$sundayStart' WHERE studentid = '$studentid'";
+	$updateSundayStop = "UPDATE employees SET sundayStop = '$sundayStop' WHERE studentid = '$studentid'";
+	
+	if ($firstname != null) {
+		$db->exec($updateFirstname);
+	}
+	if ($lastname != null) {
+		$db->exec($updateLastname);
+	}
+	if ($studentid != null) {
+		$db->exec($updateStudentid);
+	}
+	if ($phone != null) {
+		$db->exec($updatePhone);
+	}
+	if ($email != null) {
+		$db->exec($updateEmail);
+	}
+	if ($checker != null) {
+		$db->exec($updateChecker);
+	}
+	if ($role != null) {
+		$db->exec($updateRole);
+	}
+	if ($mondayStart != null) {
+		$db->exec($updateMondayStart);
+	}
+	if ($mondayStop != null) {
+		$db->exec($updateMondayStop);
+	}
+	if ($tuesdayStart != null) {
+		$db->exec($updateTuesdayStart);
+	}
+	if ($tuesdayStop != null) {
+		$db->exec($updateTuesdayStop);
+	}
+	if ($wednesdayStart != null) {
+		$db->exec($updateWednesdayStart);
+	}
+	if ($wednesdayStop != null) {
+		$db->exec($updateWednesdayStop);
+	}
+	if ($thursdayStart != null) {
+		$db->exec($updateThursdayStart);
+	}
+	if ($thursdayStop != null) {
+		$db->exec($updateThursdayStop);
+	}
+	if ($fridayStart != null) {
+		$db->exec($updateFridayStart);
+	}
+	if ($fridayStop != null) {
+		$db->exec($updateFridayStop);
+	}
+	if ($saturdayStart != null) {
+		$db->exec($updateSaturdayStart);
+	}
+	if ($saturdayStop != null) {
+		$db->exec($updateSaturdayStop);
+	}
+	if ($sundayStart != null) {
+		$db->exec($updateSundayStart);
+	}
+	if ($sundayStop != null) {
+		$db->exec($updateSundayStop);
+	}
+	
+	$db->exec('END;');
+	$db->close();
+	return "Complete";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$postData = file_get_contents('php://input');
+    $postDecoded = json_decode($postData);
+	$firstname = $postDecoded->firstname;
+	$lastname = $postDecoded->lastname;
+	$studentid = $postDecoded->studentid;
+	$phone = $postDecoded->phone;
+	$email = $postDecoded->email;
+	$checker = $postDecoded->checker;
+	$role = $postDecoded->role;
+	$mondayStart = $postDecoded->mondayStart;
+	$mondayStop = $postDecoded->mondayStop;
+	$tuesdayStart = $postDecoded->tuesdayStart;
+	$tuesdayStop = $postDecoded->tuesdayStop;
+	$wednesdayStart = $postDecoded->wednesdayStart;
+	$wednesdayStop = $postDecoded->wednesdayStop;
+	$thursdayStart = $postDecoded->thursdayStart;
+	$thursdayStop = $postDecoded->thursdayStop;
+	$fridayStart = $postDecoded->fridayStart;
+	$fridayStop = $postDecoded->fridayStop;
+	$saturdayStart = $postDecoded->saturdayStart;
+	$saturdayStop = $postDecoded->saturdayStop;
+	$sundayStart = $postDecoded->sundayStart;
+	$sundayStop = $postDecoded->sundayStop;
+    error_log("Server received student ID: " . $studentid . "\n");
+
+    $result = applyUserEdits($firstname, $lastname, $studentid, $phone, $email, $checker, $role, $mondayStart, $mondayStop, $tuesdayStart, $tuesdayStop, $wednesdayStart, $wednesdayStop, $thursdayStart, $thursdayStop, $fridayStart, $fridayStop, $saturdayStart, $saturdayStop, $sundayStart, $sundayStop);
+
+    $jsonToSend = json_encode($result);
+    error_log("Server sending response: " . $jsonToSend);
+    echo $jsonToSend;
+}
+
+?>
